54db5d5d908e89b14a3592bafcda3b94ceb7934b
yusuf wibisono <yusufw2429@gmail.com>
2021-08-09 17:33:00 +0700

    add secure checkout and log

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -122,7 +122,7 @@ class AuthController extends Controller
     protected function createNewToken($token){
         Log::debug('function: createNewToken');
         return response()->json([
-            'access_token' => $token,
+            'access_token' => $token, 
             'token_type' => 'bearer',
             'expires_in' => auth('api')->factory()->getTTL() * 60,
             'user' => auth('api')->user()
